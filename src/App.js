3c2e008090e2dab5a2395fe2aa4a5cb54f8392b0
Shorn Jacob <shorn.jacob1986@gmail.com>
2019-01-16 17:52:48 +1300

    Changed App to be a JSX element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import Header from './components/Header'
 
-//JSX expressions become regular JavaScript function calls and evaluate to JavaScript objects.
-const App = () => (
-    <div>
-        <Header />
-    </div>
-)
+
+const App = () =>    <div><Header /></div>
+
 
 export default App
