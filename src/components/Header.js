eab4f927ae3f317be64cc52593946c200378e622
Shorn Jacob <shorn.jacob1986@gmail.com>
2019-01-16 17:42:18 +1300

    Edited App.js and Added Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Alert } from 'reactstrap';
+import { Badge } from 'reactstrap';
+
+
+const Header = () => (
+    <div>
+       <Alert color="primary">
+        Heading Alert
+      </Alert>
+    </div>
+)
+
+export default Header
