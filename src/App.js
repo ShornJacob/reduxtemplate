f9323daff43fe16e1345396d0af1cea67440c29f
Shorn Jacob <shorn.jacob1986@gmail.com>
2019-01-16 18:29:37 +1300

    Description of a possible error added in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Header from './components/Header'
 
-
+//return is implicit in arrow function
+//App =  <div><Header /></div>
+//omitting arrow function will cause error. "Check the render method of `App`."
+//Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: object.
 const App = () =>    <div><Header /></div>
-
 
 export default App
