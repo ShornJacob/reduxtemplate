004a2ff3ce1792f2dc3dcfcfb6ec19d0fe937533
Shorn Jacob <shorn.jacob1986@gmail.com>
2019-01-23 16:44:17 +1300

    Added a test for the Taskform reducer

--- src/reducers/taskReducer.test.js
+++ src/reducers/taskReducer.test.js
@@ -1,5 +1,6 @@
 import taskReducer from './taskReducer'
 import { ADDTASK } from '../actions'
+import { ADD_FORM_TASK } from '../actions'
 
 describe('post reducer tests', () => {
 
@@ -33,5 +34,25 @@ describe('post reducer tests', () => {
         ]
 
         expect(taskReducer(undefined, ADDTASK())).toEqual(newState)
+    })
+
+    test('action ADD_FORM_TASK adds the task', () => {
+
+        const newState = [
+            {
+                userId: 123,
+                id: 456,
+                title: "WOher COmest du",
+                body: "We comest azherbaijan"
+            },
+            {
+                userId: 345,
+                id: 678,
+                title: "Rex Der",
+                body: "Cedf FGG"
+            }        
+        ]
+
+        expect(taskReducer(undefined, ADD_FORM_TASK (123,456,"WOher COmest du","We comest azherbaijan"))).toEqual(newState)
     })
 })
