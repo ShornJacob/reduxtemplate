6e7742045da6b5290b3971f73f91ca4bcebc4c8e
Shorn Jacob <shorn.jacob1986@gmail.com>
2019-01-23 15:24:02 +1300

    Added a renderInput component for TaskForm

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
-import { Label, Button, Input, Alert} from 'reactstrap';
+import { Label, Button, Input } from 'reactstrap';
+
+
+const renderInput = ({input,meta,label,placeholder,type}) => 
+<div>
+  <Label>{label}</Label>
+  <Input {...input} type={type} placeholder={placeholder} />
+</div>
 
 let TaskForm = ({handleSubmit}) => 
   <form>
     <div>
-      <Field name="userId" label="User ID" component="input" type={"number"} placeholder="User Id" />
-      <Field name="id" label="ID" component="input" type={"number"} placeholder="ID" />
-      <Field name="title" label="title" component="title" type={"number"} placeholder="Title" />
-      <Field name="body" label="body" component="body" type={"number"} placeholder="First Name" />
+      <Field name="userId" label="User ID" component={renderInput} type="number" placeholder="User ID"  />
+      <Field name="id" label="ID" component={renderInput}  type="number" placeholder="ID" />
+      <Field name="title" label="title" component={renderInput} type="text" placeholder="Title" />
+      <Field name="body" label="body" component={renderInput} type="text" placeholder="First Name" />
 
       <Button  type="submit" >Submit</Button>
     </div>
